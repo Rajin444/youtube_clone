52ac74bf3fb980680a6298dde811205176215c18
rajin <hajin0533@gamil.com>
2022-09-04 18:08:53 +0900

    rajin
    유튜브 클론코딩 :root를 이용해 css 공통작업을 진행하고 video로 영상을 올려 동작할 수 있게 했습니다.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const moreBtn = document.querySelector('.info .metadata .moreBtn');
+const title = document.querySelector('.info .metadata .title');
+
+console.log(title);
+console.log(moreBtn);
+
+moreBtn.addEventListener('click', () => {
+    moreBtn.classList.toggle('clicked');
+    title.classList.toggle('clamp');
+});
